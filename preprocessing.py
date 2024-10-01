f4e4ea0489f196416a6d9b84201f59d494a4aebe
NemeWolf <nehemiasman_8@live.com>
2024-09-30 21:28:43 -0300

    Preprocessing Song Dataset for Melody Generation, pt. 2

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,8 @@ First, load the dataset of folk songs in kern format.
 Second, convert the songs from kern format to music21 score object.
 Third, filter out songs that have non-acceptable durations.
 Fourth, transpose songs to a common key.
+Fifth, encode songs with music time series representation.
+Finally, save the encoded songs to text file.
 """
 
 """
@@ -19,16 +21,19 @@ Usefull for representation of music data
 """
 
 KERN_DATASET_PATH = "G:\\My Drive\\Cursos\\Valerio_Velardo\\Melody_generation_with_RNN-LSTM\\dataset_preprocessing\\deutschl\\test"
+SAVE_DIR = "G:\\My Drive\\Cursos\\Valerio_Velardo\\Melody_generation_with_RNN-LSTM\\dataset"
+
+# durations are expressed in quarter length
 ACCEPTABLE_DURATIONS = [
-    0.25, 
-    0.5, 
-    0.75, 
-    1.0, 
-    1.5, 
-    2, 
-    3, 
-    4
-    ]
+    0.25, # 16th note
+    0.5, # 8th note
+    0.75,
+    1.0, # quarter note
+    1.5,
+    2, # half note
+    3,
+    4 # whole note
+]
 
 def load_song_in_kern(dataset_path):
     """Loads all kern pieces in dataset using music21.
@@ -65,6 +70,12 @@ def has_acceptable_durations(song, acceptable_durations):
     return True   
             
 def transpose(song):
+    """Transposes song to C maj/A min
+
+    :param piece (m21 stream): Piece to transpose
+    :return transposed_song (m21 stream):
+    """
+    
     # get key drom de the song
     
     parts = song.getElementsByClass(m21.stream.Part)    # get all the parts of the song (list)
@@ -74,9 +85,7 @@ def transpose(song):
     # estimate key using music21 (in case key is not available in the kern file)
     if not isinstance(key, m21.key.Key):
         key = song.analyze("key")
-    
-    print(f"Original key: {key}")
-    
+        
     # get interval for transposition. E.g., Bmaj -> Cmaj is a semitone up, so interval is 1
     if key.mode == "major":
         interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("C"))
@@ -88,15 +97,52 @@ def transpose(song):
     
     return transposed_song
     
-def preprocess(dataset_path):
-    pass
+def encode_song(song, time_step = 0.25):
+    """Converts a score into a time-series-like music representation. Each item in the encoded list represents 'min_duration'
+    quarter lengths. The symbols used at each step are: integers for MIDI notes, 'r' for representing a rest, and '_'
+    for representing notes/rests that are carried over into a new time step. Here's a sample encoding:
 
+        ["r", "_", "60", "_", "_", "_", "72" "_"]
+
+    :param song (m21 stream): Piece to encode
+    :param time_step (float): Duration of each time step in quarter length
+    :return:
+    """
+    # p = 60, d = 1.0 -> [60, "_", "_", "_"]  
+    
+    encoded_song = []
+    
+    for event in song.flatten().notesAndRests:
+        # handle notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi #60
+        
+        # handle rests
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+                        
+        # convert the note/rest into time series representation        
+        steps =int(event.duration.quarterLength / time_step)
+
+        for step in range(steps):
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+                
+    # cast encoded song to a str
+    encoded_song = " ".join(map(str, encoded_song))
+    
+    return encoded_song
+
+def preprocess(dataset_path):
+    
     # load the folk song
     print("Loading songs...")
     songs = load_song_in_kern(KERN_DATASET_PATH)
     print(f"Loaded {len(songs)} songs.")
     
-    for song in songs:
+    for i, song in enumerate(songs):
         
         # filter out songs that have non-aceptable duration
         if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
@@ -106,20 +152,25 @@ def preprocess(dataset_path):
         song = transpose(song)
         
         # encode songs with music time series representation
+        encoded_song = encode_song(song)
         
         # save songs to text file
-    
+        save_path = os.path.join(SAVE_DIR, str(i))
+        
+        with open(save_path, "w") as fp:
+            fp.write(encoded_song)
+        
+        
 if __name__==  "__main__":
     
     songs = load_song_in_kern(KERN_DATASET_PATH)
     print(f"Loaded {len(songs)} songs.")
     song = songs[0]
     
-    print(f"Has acceptable duration? {has_acceptable_durations(song, ACCEPTABLE_DURATIONS)}")
+    preprocess(KERN_DATASET_PATH)
     
+    # transpose song
     transposed_song = transpose(song)
-    
-    song.show() # .show is a method of music21 that shows the music score
-    transposed_song.show()
+    #transposed_song.show()
     
     
